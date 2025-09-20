242296f48c031dca6ea9b17ce0d7c0ccf5581e96
Pedro Odake <pedroodake@gmail.com>
2025-09-20 20:48:07 -0300

    rota sendo mudada

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ if __name__ == "__main__":
     
     # Use debug=False for production
     debug_mode = os.environ.get('DEBUG', 'False').lower() == 'true'
-    app.run(host="0.0.0.0", port=5000, debug=debug_mode)
+    app.run(host="0.0.0.0", port=5001, debug=debug_mode)
